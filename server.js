7ad47b97e2fb369a7331c233b567f45c730f4c09
Kdu <olocgame@gmail.com>
2023-08-10 14:31:33 -0300

    Retirando tratamento de erro

--- server.js
+++ server.js
@@ -25,25 +25,21 @@ async function createImage(qr_src, cor) {
 
     const colorImage = new Jimp(width, height, cor);
 
-    try {
-        const qrImage = await Jimp.read(Buffer.from(qr_src.split(',')[1], 'base64'));
-        qrImage.resize(700, 700, Jimp.RESIZE_NEAREST_NEIGHBOR);
-        // Calcula a posição para centralizar a imagem no canvas
-        var x; 
-        var y; 
-        x = (width - qrImage.bitmap.width) / 2;
-        y = (height - qrImage.bitmap.height) / 1.5;
+    
+    const qrImage = await Jimp.read(Buffer.from(qr_src.split(',')[1], 'base64'));
+    qrImage.resize(700, 700, Jimp.RESIZE_NEAREST_NEIGHBOR);
+    // Calcula a posição para centralizar a imagem no canvas
+    var x; 
+    var y; 
+    x = (width - qrImage.bitmap.width) / 2;
+    y = (height - qrImage.bitmap.height) / 1.5;
 
-        // Combina as imagens
-        colorImage.composite(qrImage, x, y, Jimp.RESIZE_NEAREST_NEIGHBOR);
+    // Combina as imagens
+    colorImage.composite(qrImage, x, y, Jimp.RESIZE_NEAREST_NEIGHBOR);
 
-        // Converte a imagem completa para base64
-        const finalBase64Image = await colorImage.getBase64Async(Jimp.MIME_PNG);
-        return finalBase64Image;
-    } catch (error) {
-        console.error("Erro ao criar a imagem:", error);
-        throw error;
-    }
+    // Converte a imagem completa para base64
+    const finalBase64Image = await colorImage.getBase64Async(Jimp.MIME_PNG);
+    return finalBase64Image;
 }
 
 app.post('/', async (req, res) => {
